64fbfdbbec2c41c74dda555d9499117cedb4ac81
Cory-Watt <106133667+Cory-Watt@users.noreply.github.com>
2023-08-13 05:02:02 +0000

    .

--- jokes-basic/db_connect.php
+++ jokes-basic/db_connect.php
@@ -4,7 +4,7 @@ ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
 $AZURE_MYSQL_HOST = "topic6-server.mysql.database.azure.com";
-$AZURE_MYSQL_PORT = "3306"; // If you're using the default port (3306) for MySQL, then you might not actually need this line.
+$AZURE_MYSQL_PORT = "3306";
 $AZURE_MYSQL_USERNAME = "cory";
 $AZURE_MYSQL_PASSWORD = "L3T0H27KBRHX76IP$";
 $AZURE_MYSQL_DATABASE = "jokes";
